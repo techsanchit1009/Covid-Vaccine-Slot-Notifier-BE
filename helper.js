542a7e54b8d10060d6320390966ec5d092d3cf62
Sanchit Sachdeva <sanchit.sachdeva@tothenew.com>
2021-05-09 14:12:53 +0530

    initial commit

---
+++ helper.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const moment = require('moment');
+const telegramBot = require('telegram-bot-api');
+const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+
+const tgApi = new telegramBot({
+  token: process.env.TELEGRAM_BOT_TOKEN
+});
+
+const checkAvailability = async (pincode, minAge) => {
+  const date = moment().format('DD-MM-YYYY');
+  const response = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`, {
+    headers: {
+      Accept: '*/*',
+      'User-Agent': '*',
+      'Content-Type': 'application/json'
+    }
+  });
+  if (response && response.data) {
+    const centers = response.data.centers;
+    const filteredCenters = centers.map(center => {
+      return {
+        ...center,
+        sessions: center.sessions.filter(
+          (session) =>
+            session.available_capacity > 0 && session.min_age_limit === minAge
+        ),
+      };
+    });
+    const availableCenters = [];
+    filteredCenters.forEach(center => {
+      if (center.sessions.length > 0) {
+        availableCenters.push(center);
+      }
+    });
+    if (availableCenters.length > 0) {
+      let smsString = '';
+      availableCenters.forEach((center, i) => {
+        smsString = smsString.concat(`<b>${center.name}</b> -- (<b>${minAge}+</b>) -- ${center.fee_type} \n`);
+      });
+      // await client.messages.create({
+      //   body: '\nAvailable centers -- \n'.concat(smsString),
+      //   from: '+17727948273',
+      //   to: '+919899855244',
+      // });
+      // client.messages.create({
+      //   from: 'whatsapp:+14155238886',
+      //   to: 'whatsapp:+919899855244',
+      //   body: '\nAvailable centers -- \n'.concat(smsString),
+      // });
+      tgApi.sendMessage({
+        chat_id: '@cowinnotifier',
+        text: `<u><b>${moment().format('Do MMM YYYY')}</b> - Available centers for <b>${pincode}</b> \n\n</u>`.concat(smsString),
+        parse_mode: 'html'
+      });
+    }
+    return [...availableCenters];
+  } else {
+    return { message: 'No data found' };
+  }
+}
+
+exports.checkAvailability = checkAvailability;
---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+require('dotenv').config();
+const httpServer = http.createServer(app);
+const bodyParser = require('body-parser');
+const cron = require('node-cron');
+const helper = require('./helper');
+
+const availablePincodes = ['110095', '110092', '110040'];
+const selectedAgeGroup = [18, 45];
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, HEAD, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+  next();
+});
+
+app.use(bodyParser.json())
+
+app.get("/", async (req, res) => {
+  try {
+    availablePincodes.forEach(pincode => {
+      selectedAgeGroup.forEach(async (minAge) => {
+        await helper.checkAvailability(pincode, minAge);
+      } )
+    });
+    res.send({data: 'Messages sent'});
+  } catch (err) {
+    console.log('err');
+  }
+});
+
+app.post("/add-pincode", (req, res) => {
+  if (!availablePincodes.includes(req.body.pincode)) {
+    availablePincodes.push(req.body.pincode);
+  }
+  res.send({pincodes: availablePincodes});
+});
+
+// runs every 10 minutes
+cron.schedule(process.env.CRON_CONFIG, async () => {
+  console.log('CRON EXECUTED');
+  availablePincodes.forEach(pincode => {
+    selectedAgeGroup.forEach(async (minAge) => {
+      await helper.checkAvailability(pincode, minAge);
+    })
+  });
+});
+
+httpServer.listen(process.env.PORT, () => {
+  console.log("Server running at", process.env.PORT);
+});
